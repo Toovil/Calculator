43eeec81bbb858a9fc6e2c1b1562fe9b74107204
Juan <jujotovil@gmail.com>
2021-09-19 18:30:27 -0500

    First commit

---
+++ src/Main/Calculator.java
@@ -0,0 +1,16 @@
+
+package Main;
+
+import View.Vista;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+       Vista pantalla = new Vista();
+       pantalla.setVisible(true);
+       
+       
+     
+    }
+    
+}
